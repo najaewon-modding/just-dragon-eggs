58601288060e614f3db76074d43fa4b62bd6cfa6
JWN <89252111+skwodnjs@users.noreply.github.com>
2026-09-07 23:50:55 +0900

    Use standard BlockItem for recorded egg

--- src/main/java/net/njw/justdragoneggs/registry/ModContent.java
+++ src/main/java/net/njw/justdragoneggs/registry/ModContent.java
@@ -4,6 +4,7 @@ import java.util.function.Supplier;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -15,7 +16,6 @@ import net.njw.justdragoneggs.JustDragonEggs;
 import net.njw.justdragoneggs.block.RecordedDragonEggBlock;
 import net.njw.justdragoneggs.block.entity.RecordedDragonEggBlockEntity;
 import net.njw.justdragoneggs.dragon.DragonBattleRecord;
-import net.njw.justdragoneggs.item.RecordedDragonEggItem;
 
 public final class ModContent {
     private static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(JustDragonEggs.MODID);
@@ -24,7 +24,7 @@ public final class ModContent {
     private static final DeferredRegister.DataComponents DATA_COMPONENTS = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, JustDragonEggs.MODID);
 
     public static final DeferredBlock<RecordedDragonEggBlock> RECORDED_DRAGON_EGG = BLOCKS.registerBlock("recorded_dragon_egg", RecordedDragonEggBlock::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.DRAGON_EGG));
-    public static final DeferredItem<RecordedDragonEggItem> RECORDED_DRAGON_EGG_ITEM = ITEMS.registerItem("recorded_dragon_egg", properties -> new RecordedDragonEggItem(RECORDED_DRAGON_EGG.get(), properties.useBlockDescriptionPrefix()));
+    public static final DeferredItem<BlockItem> RECORDED_DRAGON_EGG_ITEM = ITEMS.registerItem("recorded_dragon_egg", properties -> new BlockItem(RECORDED_DRAGON_EGG.get(), properties.useBlockDescriptionPrefix()));
     public static final Supplier<BlockEntityType<RecordedDragonEggBlockEntity>> RECORDED_DRAGON_EGG_BLOCK_ENTITY = BLOCK_ENTITY_TYPES.register("recorded_dragon_egg", () -> new BlockEntityType<>(RecordedDragonEggBlockEntity::new, RECORDED_DRAGON_EGG.get()));
     public static final Supplier<DataComponentType<DragonBattleRecord>> BATTLE_RECORD = DATA_COMPONENTS.registerComponentType("battle_record", builder -> builder.persistent(DragonBattleRecord.CODEC).networkSynchronized(ByteBufCodecs.fromCodecWithRegistries(DragonBattleRecord.CODEC)));
 
